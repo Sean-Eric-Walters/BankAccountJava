79637eb39de10ddfe1296ecb2808e68aaae81457
Sean-Eric-Walters <83595394+Sean-Eric-Walters@users.noreply.github.com>
2021-08-06 13:39:09 -0700

    Add files via upload

---
+++ bankAcount/BankAccountTest.java
@@ -0,0 +1,17 @@
+package com.sean.bankAcount;
+
+
+public class BankAccountTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BankAccount test = new BankAccount();
+		test.deposit(100, "checking");
+		test.deposit(100, "savings");
+		test.getAllMoney();
+		test.withdraw(50, "checking");
+		test.getBothAccAmounts();
+		test.getAllAccounts();
+	}
+
+}
